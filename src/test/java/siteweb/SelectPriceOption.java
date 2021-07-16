a5c74032d8b7901f70ce4324a14dfdd9a16a0402
Davifreitas0308 <davifreitas0308@gmail.com>
2021-07-16 17:56:51 -0300

    Alteração nas Classes SendQuote e PriceOption
    Incluido alguns elementos

--- src/test/java/siteweb/SelectPriceOption.java
+++ src/test/java/siteweb/SelectPriceOption.java
@@ -1,5 +1,13 @@
 package siteweb;
 
+import org.openqa.selenium.WebDriver;
+
 public class SelectPriceOption {
+	
+	WebDriver driver;
+	public SelectPriceOption(WebDriver driver)
+	{
+	this.driver = driver;	
+	}
 
 }
--- src/test/java/siteweb/SendQuote.java
+++ src/test/java/siteweb/SendQuote.java
@@ -1,5 +1,13 @@
 package siteweb;
 
+import org.openqa.selenium.WebDriver;
+
 public class SendQuote {
+	
+	WebDriver driver;
+	public SendQuote (WebDriver driver)
+	{
+	this.driver = driver;	
+	}
 
 }
